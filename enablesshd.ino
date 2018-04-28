7b29ef21ec3af2d92ce4cd3e7a24e44c019f52e6
Clif Wolfe <34320249+flounderK@users.noreply.github.com>
2018-04-27 22:23:21 -0400

    Added enable sshd.ino

---
+++ enablesshd.ino
@@ -0,0 +1,34 @@
+#include <Keyboard.h>
+void setup() {
+  // put your setup code here, to run once:
+  for (int i=0;i <= 130; i++){
+    delay(1000);
+  }
+  
+  Keyboard.println("pi\n");
+  delay(3000);
+  Keyboard.println("raspberry\n");
+  delay(3000);
+  Keyboard.println("sudo -s\n");
+  delay(3000);
+  //Keyboard.println("raspberry\n");
+  delay(3000);
+  Keyboard.println("systemctl enable ssh\n");
+  delay(3000);
+  Keyboard.println("systemctl start ssh\n");
+  delay(3000);
+  Keyboard.println("exit\n");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+
+
+
+
+
+
