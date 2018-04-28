0088753dd3796f1906a4616441a753c0cfc53034
flounderK <clifwolfe1@gmail.com>
2018-04-28 17:27:47 -0400

    Added initial config for raspberry pi zero, and blinking light code to enablewifi file

--- Raspberry pi stuff/Pi_Zero_initial_setup/Pi_Zero_initial_setup.ino
+++ Raspberry pi stuff/Pi_Zero_initial_setup/Pi_Zero_initial_setup.ino
@@ -1,9 +1,10 @@
 #include <Keyboard.h>
-
 //LED blinking code found at https://learn.sparkfun.com/tutorials/pro-micro--fio-v3-hookup-guide/all
 int RXLED = 17;
+
 void setup() {
   // put your setup code here, to run once:
+  //ample time for the system to fully start
   for (int i=0;i <= 130; i++){
     delay(1000);
   }
@@ -14,12 +15,19 @@ void setup() {
   delay(3000);
   Keyboard.println("sudo -s\n");
   delay(3000);
-  Keyboard.println("printf 'allow-hotplug wlan0\nauto wlan0\niface wlan0 inet dhcp\nwpa-ssid \"ssid\"\nwpa-psk \"password\"\n' >> /etc/network/interfaces \n");
+  Keyboard.println("systemctl enable ssh\n");
+  delay(6000);
+  Keyboard.println("systemctl start ssh\n");
   delay(3000);
-  //this will fail.
-  //Keyboard.println("systemctl restart networking.service\n");
-  //delay(3000);
+  //fix the keyboard layout. 
+  Keyboard.println("sed -i s/gb/us/ /etc/default/keyboard\n");
+  delay(3000);
+  //add in the wifi. The @ signs are there because the keyboard layout is set to gb until reboot. 
+  Keyboard.println("printf 'allow-hotplug wlan0\nauto wlan0\niface wlan0 inet dhcp\nwpa-ssid @ssid@\nwpa-psk @password@\n' >> /etc/network/interfaces \n");
+  delay(3000);
   Keyboard.println("exit\n");
+  pinMode(RXLED, OUTPUT);  // Set RX LED as an output
+  // TX LED is set as an output behind the scenes
 }
 
 void loop() {
