74d413cea437757e52e40f0253e1c5bc39b70574
flounderK <clifwolfe1@gmail.com>
2018-04-28 15:57:35 -0400

    Made the code work from startup

--- Raspberry pi stuff/enableWifi/enableWifi.ino
+++ Raspberry pi stuff/enableWifi/enableWifi.ino
@@ -1,15 +1,20 @@
 #include <Keyboard.h>
 void setup() {
   // put your setup code here, to run once:
-
+  for (int i=0;i <= 130; i++){
+    delay(1000);
+  }
+  
+  Keyboard.println("pi\n");
+  delay(3000);
+  Keyboard.println("raspberry\n");
   delay(3000);
   Keyboard.println("sudo -s\n");
   delay(3000);
   Keyboard.println("printf 'allow-hotplug wlan0\nauto wlan0\niface wlan0 inet dhcp\nwpa-ssid \"ssid\"\nwpa-psk \"password\"\n' >> /etc/network/interfaces \n");
   delay(3000);
-  Keyboard.println("exit\n");
-  //Keyboard.println("systemctl reboot\n");
-
+  Keyboard.println("systemctl reboot\n");
+  //Keyboard.println("exit\n");
 }
 
 void loop() {
